eb4e96019921e2e9796e97c37e5d4820a52a9acd
Dirk <info@videohoohaa.com>
2014-06-07 18:36:33 -0400

    Adding RepositoryKit

---
+++ Frameworks/RepositoryKit/RepositoryKit/RepositoryKit/RepositoryKit.h
@@ -0,0 +1,19 @@
+//
+//  RepositoryKit.h
+//  RepositoryKit
+//
+//  Created by Dirk Lewis on 6/7/14.
+//  Copyright (c) 2014 VideoHooHaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RepositoryKit.
+FOUNDATION_EXPORT double RepositoryKitVersionNumber;
+
+//! Project version string for RepositoryKit.
+FOUNDATION_EXPORT const unsigned char RepositoryKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RepositoryKit/PublicHeader.h>
+
+
